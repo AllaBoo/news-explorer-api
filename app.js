cb634f002742e4557b43e5a0b82e03b872c56264
AllaBoo <a.v.buteeva@gmail.com>
2020-10-03 22:43:34 +0300

    адрес базы данных вынесен в process.env

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const rateLimiter = require('./middlewares/rateLimiter');
 const router = require('./routes');
 const { errorHandler } = require('./middlewares/errorHandler');
 
-mongoose.connect('mongodb://localhost:27017/news-db', {
+const { NODE_ENV, DATABASE_URL } = process.env;
+
+mongoose.connect(NODE_ENV === 'production' ? DATABASE_URL : 'mongodb://localhost:27017/news-db', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
